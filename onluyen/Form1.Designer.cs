598233fe06a8ee64b11455ff833bb962bf352c24
Việt Nguyễn <godofwisdomm@gmail.com>
2024-03-30 20:29:11 +0700

    Add project files.

---
+++ onluyen/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace onluyen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblMa = new System.Windows.Forms.Label();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblLoai = new System.Windows.Forms.Label();
+            this.dataG = new System.Windows.Forms.DataGridView();
+            this.cmbLKH = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(24, 120);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(36, 20);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Tên";
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(103, 117);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblMa
+            // 
+            this.lblMa.AutoSize = true;
+            this.lblMa.Location = new System.Drawing.Point(29, 71);
+            this.lblMa.Name = "lblMa";
+            this.lblMa.Size = new System.Drawing.Size(31, 20);
+            this.lblMa.TabIndex = 2;
+            this.lblMa.Text = "Mã";
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(103, 71);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(100, 26);
+            this.txtMa.TabIndex = 3;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(346, 24);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(55, 20);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Phone";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(475, 21);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 26);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // lblLoai
+            // 
+            this.lblLoai.AutoSize = true;
+            this.lblLoai.Location = new System.Drawing.Point(346, 71);
+            this.lblLoai.Name = "lblLoai";
+            this.lblLoai.Size = new System.Drawing.Size(65, 20);
+            this.lblLoai.TabIndex = 6;
+            this.lblLoai.Text = "Loại KH";
+            // 
+            // dataG
+            // 
+            this.dataG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataG.Location = new System.Drawing.Point(188, 226);
+            this.dataG.Name = "dataG";
+            this.dataG.RowHeadersWidth = 62;
+            this.dataG.RowTemplate.Height = 28;
+            this.dataG.Size = new System.Drawing.Size(240, 150);
+            this.dataG.TabIndex = 7;
+            this.dataG.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataG_CellContentClick);
+            // 
+            // cmbLKH
+            // 
+            this.cmbLKH.FormattingEnabled = true;
+            this.cmbLKH.Location = new System.Drawing.Point(475, 68);
+            this.cmbLKH.Name = "cmbLKH";
+            this.cmbLKH.Size = new System.Drawing.Size(121, 28);
+            this.cmbLKH.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbLKH);
+            this.Controls.Add(this.dataG);
+            this.Controls.Add(this.lblLoai);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.lblMa);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblMa;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblLoai;
+        private System.Windows.Forms.DataGridView dataG;
+        private System.Windows.Forms.ComboBox cmbLKH;
+    }
+}
+
---
+++ onluyen/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace onluyen
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        string Constring = "Data Source=DESKTOP-93NLVCJ;Initial Catalog=QLkhachhang;Integrated Security=True";
+        SqlCommand cmd;
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void loaddata()
+        {
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from TB_KhachHang";
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataG.DataSource = dt;
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(Constring);
+            conn.Open();
+            loaddata();
+        }
+
+        private void dataG_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataG.CurrentRow.Index;
+            txtMa.Text = dataG.Rows[i].Cells[0].Value.ToString();
+            txtName.Text = dataG.Rows[i].Cells[1].Value.ToString();
+        }
+    }
+}
